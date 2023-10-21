44a0574ed03397ab4a272aa0f9de758f533a1aff
Ilya Piskurov <piskryov@gmail.com>
2023-10-21 15:52:01 +0300

    add static folder

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static',
+            template_folder='templates')
 
 @app.route('/')
 def home() -> str:
-    return 'Hello, World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
